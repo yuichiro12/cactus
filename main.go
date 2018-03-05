9b182094f096be93d4a25c88a80343d09b6085b2
yuichiro12 <talcnrhyme@gmail.com>
2018-03-05 20:52:23 +0900

    remove say command pipe and automate

--- main.go
+++ main.go
@@ -3,38 +3,50 @@ package main
 import (
 	"fmt"
 	"log"
+	"os/exec"
 	"time"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 func main() {
-	doc, err := goquery.NewDocument("https://hckrnews.com")
-	if err != nil {
-		log.Fatal(err)
-	}
+	for {
+		fmt.Println("Getting articles from hckrnews.com...")
+		doc, err := goquery.NewDocument("https://hckrnews.com")
+		if err != nil {
+			log.Fatal(err)
+		}
 
-	doc.Find("a.link").EachWithBreak(func(i int, s *goquery.Selection) bool {
-		if i > 10 {
-			return false
-		}
-		link, exists := s.Attr("href")
-		if exists {
-			readArticle(link)
-		}
-		time.Sleep(3 * time.Second)
-		return true
-	})
+		doc.Find("a.link").EachWithBreak(func(i int, s *goquery.Selection) bool {
+			if i > 5 {
+				return false
+			}
+			link, exists := s.Attr("href")
+			title := s.Text()
+			if exists {
+				readArticle(title, link)
+			}
+			time.Sleep(3 * time.Second)
+			return true
+		})
+	}
 }
 
-func readArticle(link string) {
+func readArticle(title string, link string) {
 	doc, err := goquery.NewDocument(link)
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	fmt.Printf("%s --\n", doc.Find("h1").Text())
+	fmt.Printf("%s -- %s\n", title, link)
+	err = exec.Command("say", title).Run()
+	if err != nil {
+		log.Fatal(err)
+	}
 	doc.Find("p").Each(func(i int, s *goquery.Selection) {
-		fmt.Printf("%s\n", s.Text())
+		err = exec.Command("say", s.Text()).Run()
+		if err != nil {
+			log.Fatal(err)
+		}
 	})
 }
